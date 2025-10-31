32dfa080a88f3e67e55d851d91e388e673c806c2
kieudinhvan2307 <kieudinhvana4k51@gmail.com>
2025-10-31 23:29:49 +0700

    remove public/css

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources','views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () => {
+  console.log(`✅ Server is running at http://localhost:${port}`);
+});
