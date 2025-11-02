2b7aeb2387593f5442a2bb56f644cff7a8cb643c
kieudinhvan2307 <kieudinhvana4k51@gmail.com>
2025-11-02 17:13:53 +0700

    install prettier,lint-staged,husky

--- src/index.js
+++ src/index.js
@@ -4,23 +4,27 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 const path = require('path');
+const route = require('./route');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 //HTTP logger
 app.use(morgan('combined'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 //Template engine
-app.engine('hbs', handlebars.engine({
-    extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources','views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+route(app);
 
 app.listen(port, () => {
-  console.log(`✅ Server is running at http://localhost:${port}`);
+    console.log(`✅ Server is running at http://localhost:${port}`);
 });
